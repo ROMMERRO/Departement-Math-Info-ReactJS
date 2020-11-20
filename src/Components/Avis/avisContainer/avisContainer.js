53a793db3495d7d0b9ac41b37568c93efb428f4b
Mohamed OUFRAd <oufrad00@gmail.com>
2020-11-20 17:24:03 +0100

    .

--- src/Components/Avis/avisContainer/avisContainer.js
+++ src/Components/Avis/avisContainer/avisContainer.js
@@ -1,35 +1,20 @@
 import React from 'react';
 import AvisWidget from '../avisWidget/avisWidget.js';
-import AvisCard from '../avisCard/avisCard.js';
-import './avisContainer.css'
+import './avisContainer.css';
 
 class AvisContainer extends React.Component{
-    constructor(){
-        super();
-        this.state ={
-            avisCard : false
-        } 
-    }
 
-    displayAvis =()=>{
-        this.setState({avisCard : true});
-    }
     render(){
         return(
             <div className="avisConatiner">
-                {
-                    this.state.avisCard === true 
-                    ?
-                    <AvisCard />
-                    :
                     <>
-                        <AvisWidget  displayAvis={this.displayAvis}/>
-                        <AvisWidget  displayAvis={this.displayAvis}/>
-                        <AvisWidget  displayAvis={this.displayAvis}/>
-                        <AvisWidget  displayAvis={this.displayAvis}/>
-                        <AvisWidget  displayAvis={this.displayAvis}/>
+                        <AvisWidget  />
+                        <AvisWidget  />
+                        <AvisWidget  />
+                        <AvisWidget  />
+                        <AvisWidget  />
+                        <AvisWidget  />
                     </>
-                }   
             </div>
         )
     }
